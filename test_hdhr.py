c8858e4db0a6001fad9ad3ea3b52b2c642359757
Michael Mohr <akihana@gmail.com>
2015-06-13 22:43:16 -0700

    Update sample Python program

--- test_hdhr.py
+++ test_hdhr.py
@@ -5,6 +5,8 @@ from silicondust import HDHR, HDHRError
 devices = HDHR.discover()
 if len(devices) > 0:
     try:
+        devices[0].set('/tuner0/vchannel', '702')
+        print 'Tuner 0 vchannel: ' + devices[0].get('/tuner0/vchannel')
         print 'Tuner 0 channel: ' + devices[0].get('/tuner0/channel')
         print 'Tuner 0 channelmap: ' + devices[0].get('/tuner0/channelmap')
         print 'Model: ' + devices[0].get('/sys/model')
